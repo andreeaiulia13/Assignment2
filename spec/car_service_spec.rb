7ecfcd02be42f61c3f1de8361441b69ea52c30ff
andreeaiulia13 <andreeagherman1320@gmail.com>
2021-04-22 11:48:32 +0300

    Split code using MVC pattern, add unit tests, make code cleaner, add spaces between '' and do

--- spec/car_service_spec.rb
+++ spec/car_service_spec.rb
@@ -5,7 +5,7 @@ describe "check open times" do
 	context "when sunday" do
 		time = Time.new(2021, 04, 25, 9, 00, 0)
 		
-		it "return true"do
+		it "return true" do
 			expect(check_opening_time(time)).to eq (true)
 		end
 	end
@@ -13,7 +13,7 @@ describe "check open times" do
 	context "when saturday after 16:00" do
 		time = Time.new(2021, 04, 24, 16, 00, 0)
 			
-		it "return true"do
+		it "return true" do
 			expect(check_opening_time(time)).to eq (true)
 		end
 	end
@@ -21,7 +21,7 @@ describe "check open times" do
 	context "when saturday before 14:00" do
 		time = Time.new(2021, 04, 24, 13, 59, 0)
 			
-		it "return false"do
+		it "return false" do
 			expect(check_opening_time(time)).to eq (false)
 		end
 	end
@@ -29,7 +29,7 @@ describe "check open times" do
 	context "when week day before 16:00" do
 		time = Time.new(2021, 04, 23, 15, 59, 0)
 			
-		it "return false"do
+		it "return false" do
 			expect(check_opening_time(time)).to eq (false)
 		end
 	end
@@ -37,7 +37,7 @@ describe "check open times" do
 	context "when week day before 8:00" do
 		time = Time.new(2021, 04, 23, 7, 59, 0)
 			
-		it "return true"do
+		it "return true" do
 			expect(check_opening_time(time)).to eq (true)
 		end
 	end
@@ -45,11 +45,10 @@ describe "check open times" do
 	context "when week day between 8:00 and 16:00" do
 		time = Time.new(2021, 04, 23, 8, 01, 0)
 			
-		it "return false"do
+		it "return false" do
 			expect(check_opening_time(time)).to eq (false)
 		end
 	end
-
 end
 
 describe "check cars wash machines availability" do 
@@ -86,7 +85,6 @@ describe "check cars wash machines availability" do
 			expect(check_schedule(Time.now)).to eq (true)
 		end
 	end
-
 end
 
 
