c674843218556cb1347e73bf149585b97c7454d7
andreeaiulia13 <andreeagherman1320@gmail.com>
2021-04-23 11:25:03 +0300

    require_relative instead of require, single quotes where no interpolation, refactor code, split long method check_opening_time in two

---
+++ models/model.rb
@@ -0,0 +1,31 @@
+$cars = []
+
+class Car
+  attr_reader :user_name, :drop_time, :license_plate
+  attr_accessor :pickup_time
+
+  def initialize(user_name, time, license_plate)
+    @user_name = user_name
+    @drop_time = time
+    @pickup_time = (time + 7200)
+    @license_plate = license_plate
+  end
+end
+
+$cars << Car.new('Isabella Kramer', Time.new(2021, 04, 23, 8, 30, 0), 'CD80519')
+$cars << Car.new('Benn Davie', Time.new(2021, 04, 23, 9, 30, 0),'AM59232')
+$cars << Car.new('Lillian Hewitt', Time.new(2021, 04, 23, 11, 30, 0), 'BB13822')
+$cars << Car.new('Amelia Cherry', Time.new(2021, 04, 23, 11, 45, 0), 'ZK05821')
+$cars << Car.new('Benn Davie', Time.new(2021, 04, 23, 15, 00, 0), 'AN23001')
+$cars << Car.new('Adam Tristin', Time.new(2021, 04, 24, 14, 00, 0), 'TN41312')
+$cars << Car.new('Bernard Warren', Time.new(2021, 04, 24, 8, 00, 0), 'CF71829')
+
+def show_schedule 
+  puts 'OPENING TIMES'
+  puts ''
+  puts 'MONDAY - FRIDAY 8:00 - 18:00'
+  puts 'SATURDAY 8:00 - 16:00'
+  puts 'SUNDAY CLOSED'
+  puts ''
+  puts "!If you don't have an appointment, please come with two hours before closing time!" 
+end
---
+++ views/view.rb
@@ -0,0 +1,50 @@
+require_relative  '../models/model.rb'
+require_relative '../controllers/controller.rb'
+
+puts ''
+puts "        WELCOME TO OUR CAR CLEANING SERVICE!"
+puts '       Caring for your vehicle is our business'
+puts '              Clean cars run better'
+
+option = 0
+
+while(option != 6)
+
+  puts ''
+  puts 'How can we help you?'
+  puts '1. I want my car to be cleaned now'
+  puts '2. I want to make an appointment'
+  puts '3. I want to check my car cleaning status'
+  puts '4. I want to check the car cleaning service schedule'
+  puts '5. I want to check the car cleaning service appointments'
+  puts '6. Exit'
+  puts ''
+
+  option = gets.chomp
+
+  case option
+  when '1'
+    add_appointment_now
+
+  when '2'
+    make_new_appointment
+
+  when '3' 
+    check_car_status
+
+  when '4'
+    show_schedule
+
+  when '5'
+    show_appointments
+
+  when '6'
+    puts 'Thank you for choosing our car cleaning service.'
+    puts "We are waiting for you to come back, don't forget: A clean car is a happy car"
+    puts 'Have a good one!'
+    break
+
+  else
+    puts 'Please enter a number from 1-6 (1, 2, 3, 4, 5, 6)'
+  end
+end
